1cf23f62478804411afc46ead21aa915be0687ac
Dominik <77927514+OverRam@users.noreply.github.com>
2021-03-30 22:17:12 +0200

    Handle ai, change name class

---
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/IsBadCellToMark.java
@@ -0,0 +1,11 @@
+package tictactoe.Game;
+
+class IsBadCellToMark {
+    static boolean isBadCell(char[][] filedToCheck, int[] coordinates, boolean isPlayer) {
+        if (filedToCheck[coordinates[0]][coordinates[1]] == ' ') {
+            return false;
+        }
+        System.out.print(isPlayer ? "This cell is occupied! Choose another one!\n" : "");
+        return true;
+    }
+}
