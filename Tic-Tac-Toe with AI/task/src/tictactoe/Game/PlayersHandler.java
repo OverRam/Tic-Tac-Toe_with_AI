a747fa679346d9b462b0b8599e342122dd08209b
Dominik <77927514+OverRam@users.noreply.github.com>
2021-04-03 23:23:49 +0200

    adding and implementing user and AI service

---
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/PlayersHandler.java
@@ -0,0 +1,40 @@
+package tictactoe.Game;
+
+class PlayersHandler {
+    private static String MODE;
+    private static FiledGame FIELD;
+    private static int PLAYER;
+    private static char PLAYER_CHAR;
+
+    static void handler(String modePlayer, FiledGame field, int player, char playerChar) {
+        PlayersHandler.MODE = modePlayer;
+        PlayersHandler.FIELD = field;
+        PlayersHandler.PLAYER = player;
+        PlayersHandler.PLAYER_CHAR = playerChar;
+
+        switch (modePlayer) {
+            case "user":
+                userPlayer();
+                break;
+            case "easy":
+
+        }
+    }
+
+    private static void userPlayer() {
+        FIELD.setMark(GeneratorCell.playerGenerateCell(FIELD.getFieldToGame()), PLAYER_CHAR);
+    }
+
+    private static void easyAiPlayer() {
+        FIELD.setMark(GeneratorCell.aiGenerateCell(FIELD.getFieldToGame(), MODE), PLAYER_CHAR);
+    }
+
+    private static void mediumAiPlayer() {
+
+    }
+
+    private static void hardAiPlayer() {
+
+    }
+
+}
