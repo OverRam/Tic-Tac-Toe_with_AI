6f5fecfa29bdfdb843dfbafb35416f4e2cbfec9b
Dominik <77927514+OverRam@users.noreply.github.com>
2021-04-04 14:13:17 +0200

    Refactor method checkInputParams

--- Tic-Tac-Toe with AI/task/src/tictactoe/Game/CheckParamsToStartProgram.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/CheckParamsToStartProgram.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 
 class CheckParamsToStartProgram {
     static boolean checkInputParams(String[] input) {
-        if (input.length != 3 && !"start".equals(input[0])) {
-            return false;
-        }
-        return Arrays.stream(input).filter(i -> "easy".equals(i) || "user".equals(i)).count() == 2;
+        return Arrays.stream(input).filter(i -> "easy".equals(i) || "user".equals(i)).count() == 2 && input.length == 3;
     }
 }
