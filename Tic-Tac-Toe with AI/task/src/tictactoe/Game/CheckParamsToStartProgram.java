ebaa5326d3ee4cc50c14440be2d408ea7a7c5729
Dominik <77927514+OverRam@users.noreply.github.com>
2021-04-05 22:56:56 +0200

    Add medium and hard key as words in checkInputParams method

--- Tic-Tac-Toe with AI/task/src/tictactoe/Game/CheckParamsToStartProgram.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/CheckParamsToStartProgram.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 class CheckParamsToStartProgram {
     static boolean checkInputParams(String[] input) {
-        return Arrays.stream(input).filter(i -> "easy".equals(i) || "user".equals(i)).count() == 2 && input.length == 3;
+        return Arrays.stream(input).filter(i -> "easy".equals(i) || "medium".equals(i) ||
+                "hard".equals(i) || "user".equals(i)).count() == 2 && input.length == 3;
     }
 }
