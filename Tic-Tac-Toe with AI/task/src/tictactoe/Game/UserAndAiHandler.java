924ca6a6ee12af286f635ad94d2e473fd23007b5
Dominik <77927514+OverRam@users.noreply.github.com>
2021-04-06 23:10:14 +0200

    Ready medium Ai

--- Tic-Tac-Toe with AI/task/src/tictactoe/Game/UserAndAiHandler.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/UserAndAiHandler.java
@@ -1,7 +1,5 @@
 package tictactoe.Game;
 
-import java.util.Arrays;
-
 class UserAndAiHandler {
     private static FiledGame FIELD;
     private static char PLAYER_CHAR;
@@ -39,12 +37,20 @@ class UserAndAiHandler {
     }
 
     private static void mediumAiPlayer() {
-        int[] COORDINATES = aiMove(FIELD.getFieldToGame(), PLAYER_CHAR);
-        if (Arrays.stream(COORDINATES).allMatch(Character::isDigit)) {
-            FIELD.setMark(COORDINATES, PLAYER_CHAR);
+        int[] coordinatesAiToWin = aiMove(FIELD.getFieldToGame(), PLAYER_CHAR);
+        if (coordinatesAiToWin[0] < 9) {
+            //If it already has two in a row and can win with one further move, it does so.
+            FIELD.setMark(coordinatesAiToWin, PLAYER_CHAR);
         } else {
-            System.out.println("easy");
-            easyAiPlayer();
+            //If its opponent can win with one move, it plays the move necessary to block this.
+            int[] blockOponent = aiMove(FIELD.getFieldToGame(),
+                    PLAYER_CHAR == 'X' ? 'O' : PLAYER_CHAR == 'O' ? 'X' : 'O');
+            if (blockOponent[0] < 9) {
+                FIELD.setMark(blockOponent, PLAYER_CHAR);
+            } else {
+                //Otherwise, it makes a random move.
+                easyAiPlayer();
+            }
         }
     }
 
@@ -57,43 +63,54 @@ class UserAndAiHandler {
         int backSlash = 0;
         int horizontal = 0;
         int vertical = 0;
-        int[] coordinate = new int[2];
+        int[] coordinate = {9, 9};
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (i == 0) {
                     backSlash += arrayToCheck[j][j] == charToCheck ? 1 : 0;                 //check back slash
                     slash += arrayToCheck[j][2 - j] == charToCheck ? 1 : 0;                 //check slash
+
                     if (backSlash == 2) {                                                   //coordinates of back slash
                         for (int k = 0; k < 3; k++) {
-                            if (' ' == charToCheck) {
+                            if (' ' == arrayToCheck[k][k]) {
                                 coordinate[0] = k;
                                 coordinate[1] = k;
+                                return coordinate;
                             }
                         }
-                        return coordinate;
                     } else if (slash == 2) {                                                //coordinates of slash
                         for (int k = 0; k < 3; k++) {
-                            if (' ' == charToCheck) {
+                            if (' ' == arrayToCheck[k][2 - k]) {
                                 coordinate[0] = k;
                                 coordinate[1] = 2 - k;
+                                return coordinate;
                             }
                         }
-                        return coordinate;
                     }
                 }
 
                 horizontal += arrayToCheck[i][j] == charToCheck ? 1 : 0;                     //check horizontal
                 vertical += arrayToCheck[j][i] == charToCheck ? 1 : 0;                       //check vertical
-                if (horizontal == 2) {
-                    coordinate[0] = i;
-                    coordinate[1] = j;
-                    return coordinate;
-                } else if (vertical == 2) {
-                    coordinate[0] = j;
-                    coordinate[1] = i;
-                }
 
+                if (horizontal == 2) {
+                    for (int k = 0; k < 3; k++) {
+                        if (' ' == arrayToCheck[i][k]) {
+                            coordinate[0] = i;
+                            coordinate[1] = k;
+                            return coordinate;
+                        }
+                    }
+                } else if (vertical == 2) {
+                    for (int k = 0; k < 3; k++) {
+                        if (' ' == arrayToCheck[k][i]) {
+                            coordinate[0] = k;
+                            coordinate[1] = i;
+                            return coordinate;
+                        }
+                    }
+                }
             }
+
             horizontal = 0;
             vertical = 0;
         }
