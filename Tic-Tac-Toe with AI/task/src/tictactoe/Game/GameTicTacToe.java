b52e939b971b732a5d6bbdbc6cf62839dbe03b0f
Dominik <77927514+OverRam@users.noreply.github.com>
2021-04-11 13:42:06 +0200

    Moving checkWinCombination method to a new class, change name method startGame and a loop that checks the end of the game.

--- Tic-Tac-Toe with AI/task/src/tictactoe/Game/GameTicTacToe.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/GameTicTacToe.java
@@ -16,76 +16,45 @@ public class GameTicTacToe {
             inputParamsPlayer = sc.nextLine();
             playerParams = inputParamsPlayer.split(" ");
 
-            if ("exit".equals(playerParams[0])) {
+            if ("start".equals(playerParams[0])) {
+                isGoodParam = CheckParamsToStartProgram.checkInputParams(playerParams);
+            } else if ("exit".equals(playerParams[0]) && playerParams.length == 3) {
                 isGoodParam = false;
                 isPlay = false;
-            } else if ("start".equals(playerParams[0])) {
-                isGoodParam = CheckParamsToStartProgram.checkInputParams(playerParams);
+            } else {
+                System.out.print(isGoodParam ? "" : "exit".equals(playerParams[0]) ? "" : "Bad parameters!\n");
             }
 
-            System.out.print(isGoodParam ? "" : "exit".equals(playerParams[0]) ? "" : "Bad parameters!\n");
-
             if (isGoodParam) {
-                Game(new String[]{playerParams[1], playerParams[2]});
+                startGame(new String[]{playerParams[1], playerParams[2]});
             }
         }
 
         sc.close();
     }
 
-    private void Game(String[] gamers) {
+    private void startGame(String[] gamers) {
         FiledGame filedGame = new FiledGame();
         int player = 0;
         boolean isNoWin = true;
         char playerChar;
         PrintGameField.print(filedGame.getFieldToGame());
-
+        int checkWin;
         while (isNoWin) {
-            playerChar = setPlayerChar(player);
+            playerChar = player % 2 == 0 ? 'X' : 'O';
             UserAndAiHandler.handler(gamers[player % 2], filedGame, playerChar);
             PrintGameField.print(filedGame.getFieldToGame());
-            isNoWin = !checkWinCombination(filedGame.getFieldToGame(), playerChar);
+            checkWin = CheckWinCombination.checkCombination(filedGame.getFieldToGame(), playerChar);
+            isNoWin = checkWin == -1;
             player++;
 
-            if (player <= 9 && !isNoWin) {
+            if (checkWin == 1) {
                 System.out.print(playerChar + " wins\n");
                 isNoWin = false;
-            } else if (player == 9) {
+            } else if (checkWin == 0) {
                 System.out.println("Draw");
                 isNoWin = false;
             }
-
         }
-    }
-
-    private boolean checkWinCombination(char[][] arrayToCheck, char charToCheck) {
-        int slash = 0;
-        int backSlash = 0;
-        int horizontal = 0;
-        int vertical = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                horizontal += arrayToCheck[i][j] == charToCheck ? 1 : 0;                             //check horizontal
-                vertical += arrayToCheck[j][i] == charToCheck ? 1 : 0;                               //check vertical
-                if (horizontal == 3 | vertical == 3) {
-                    return true;
-                }
-                if (i == 0) {
-                    backSlash += arrayToCheck[j][j] == charToCheck ? 1 : 0;                         //check back slash
-                    slash += arrayToCheck[j][2 - j] == charToCheck ? 1 : 0;                         //check slash
-                    if (slash == 3 | backSlash == 3) {
-                        return true;
-                    }
-                }
-            }
-            horizontal = 0;
-            vertical = 0;
-        }
-        return false;
-    }
-
-    private char setPlayerChar(int player) {
-        return player % 2 == 0 ? 'X' : 'O';
     }
 }
