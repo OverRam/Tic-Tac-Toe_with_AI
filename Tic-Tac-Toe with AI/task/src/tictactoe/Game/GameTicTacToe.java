b4add08559f90191076bbf76f8171a59c64368ea
Dominik <77927514+OverRam@users.noreply.github.com>
2021-04-22 22:11:59 +0200

    securing the startGameMenu method against errors

--- Tic-Tac-Toe with AI/task/src/tictactoe/Game/GameTicTacToe.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Game/GameTicTacToe.java
@@ -6,28 +6,27 @@ public class GameTicTacToe {
 
     public void startGameMenu() {
         final Scanner sc = new Scanner(System.in);
-        String inputParamsPlayer;
         boolean isPlay = true;
         boolean isGoodParam = false;
         String[] playerParams;
 
         while (isPlay) {
             System.out.print("Input command: ");
-            inputParamsPlayer = sc.nextLine();
-            playerParams = inputParamsPlayer.split(" ");
+            playerParams = sc.nextLine().split(" ");
 
-            if ("exit".equals(playerParams[0])) {
-                isGoodParam = false;
-                isPlay = false;
-            } else if ("start".equals(playerParams[0])) {
-                isGoodParam = CheckParamsToStartProgram.checkInputParams(playerParams);
+            if (playerParams.length == 3 | (playerParams.length == 1 && playerParams[0].equals("exit"))) {
+                if ("exit".equals(playerParams[0])) {
+                    isPlay = false;
+                } else if ("start".equals(playerParams[0])) {
+                    isGoodParam = CheckParamsToStartProgram.checkInputParams(playerParams);
+                }
+
+                if (isGoodParam) {
+                    startGame(new String[]{playerParams[1], playerParams[2]});
+                }
             }
-
-            System.out.print(isGoodParam ? "" : "exit".equals(playerParams[0]) ? "" : "Bad parameters!\n");
-
-            if (isGoodParam) {
-                startGame(new String[]{playerParams[1], playerParams[2]});
-            }
+            System.out.print(isGoodParam | "exit".equals(playerParams[0]) ? "" : "Bad parameters!\n");
+            isGoodParam = false;
         }
         sc.close();
     }
@@ -37,17 +36,17 @@ public class GameTicTacToe {
         int player = 0;
         boolean isNoWin = true;
         char playerChar;
-        PrintGameField.print(filedGame.getFieldToGame());
+        PrintGameField.print(filedGame.getField());
         int checkWin;
 
         while (isNoWin) {
             playerChar = player % 2 == 0 ? 'X' : 'O';
             UserAndAiHandler.handler(gamers[player % 2], filedGame, playerChar);
-            PrintGameField.print(filedGame.getFieldToGame());
-            checkWin = CheckWinCombination.checkCombination(filedGame.getFieldToGame(), playerChar);
+            PrintGameField.print(filedGame.getField());
+            checkWin = CheckWinCombination.checkCombination(filedGame.getField(), playerChar);
             isNoWin = checkWin != -1;
             player++;
-            System.out.println(checkWin);
+
             if (checkWin == 10 || checkWin == 0) {
                 System.out.println(checkWin == 10 ? playerChar + " wins" : "Draw");
                 isNoWin = false;
